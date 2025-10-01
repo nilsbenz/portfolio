ebc3372adf93e6a7f699feee5b0edbd712f33b99
Nils Benz <nils.benz@icloud.com>
2025-10-01 18:47:54 +0200

    chore: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import tailwindcss from "@tailwindcss/vite";
-
-import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://nilsbenz.ch",
   i18n: {
     locales: ["de", "en"],
     defaultLocale: "de",
@@ -22,5 +22,17 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [react(), mdx()],
+  integrations: [
+    react(),
+    mdx(),
+    sitemap({
+      i18n: {
+        defaultLocale: "de",
+        locales: {
+          de: "de",
+          en: "en",
+        },
+      },
+    }),
+  ],
 });
