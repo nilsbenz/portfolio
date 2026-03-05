b2418c936d552c36838de29cacf983c9c479e74e
Nils Benz <nils.benz@icloud.com>
2026-03-05 09:30:28 +0100

    chore: add sommer 23 video

--- src/pages/❤️/_components/secret.tsx
+++ src/pages/❤️/_components/secret.tsx
@@ -1,67 +1,44 @@
-import { useState } from "react";
-import image from "./SNY02014.jpg";
+import { Loader2Icon, PlayIcon } from "lucide-react";
 
-function decodeBase64(base64String: string) {
-  const binaryString = atob(base64String);
-
-  const len = binaryString.length;
-  const bytes = new Uint8Array(len);
-  for (let i = 0; i < len; i++) {
-    bytes[i] = binaryString.charCodeAt(i);
-  }
-
-  return new TextDecoder("utf-8").decode(bytes);
-}
-
-const password = decodeBase64("c2Nod2VpemVyaG9m");
+const targetDate = new Date("2026-03-13" + "T00:00:00+01:00");
 
 export default function Secret() {
-  const [input, setInput] = useState("");
+  const daysUntilTargetDate = Math.max(
+    0,
+    Math.ceil((targetDate.getTime() - Date.now()) / (1000 * 60 * 60 * 24)),
+  );
 
-  if (input.toLowerCase() !== password) {
+  if (daysUntilTargetDate > 0) {
     return (
-      <div className="mx-auto flex w-full grow flex-col justify-center sm:max-w-xs">
-        <label className="flex flex-col gap-1">
-          Passwort
-          <input
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            className="bg-glass liquid-glass-border h-10 rounded-full px-6 backdrop-blur-sm focus:outline-none"
-          />
-        </label>
+      <div className="text-muted-foreground flex grow flex-col items-center justify-center gap-3">
+        <Loader2Icon
+          className="size-5 animate-spin opacity-50"
+          strokeWidth={2.5}
+        />
+        <p className="font-medium">
+          No {daysUntilTargetDate} {daysUntilTargetDate > 1 ? "Täg" : "Tag"}{" "}
+          warta...
+        </p>
       </div>
     );
   }
 
   return (
-    <div className="mx-auto w-full max-w-xl space-y-8 text-lg">
-      <h1 className="text-2xl font-semibold">
-        {decodeBase64("TMO8YmkgTWFub24g4p2k77iP")}
-      </h1>
-      <div className="-mx-4 md:-mx-8">
-        <img
-          src={image.src}
-          alt=""
-          className="bg-glass w-full backdrop-blur-sm"
-          style={{ aspectRatio: `${image.width} / ${image.height}` }}
-        />
+    <div className="-mx-2 flex grow flex-col justify-center">
+      <div className="relative isolate overflow-clip rounded-xl">
+        <div className="bg-muted absolute inset-0 -z-10 grid animate-pulse place-items-center">
+          <PlayIcon className="text-muted-foreground size-8" strokeWidth={3} />
+        </div>
+        <iframe
+          src="https://www.youtube.com/embed/qcQbXdfDvu0"
+          title="Sommer '23"
+          allow="encrypted-media; gyroscope"
+          frameBorder={0}
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+          className="aspect-video shadow-lg"
+        ></iframe>
       </div>
-      <p>
-        {decodeBase64(
-          "U2NodSB3aWRlciBuZWlndCBzaWNoIMO8c2VycyBXdWNoZWVuZCBkbyBpIGRhIExlbnplcmhlaWQgYW0gRW5kIHp1YS4gSSBob2ZmIG1lciBrb25uZCBldHogc2NodSB1ZiB6d28gdWggc2Now7ZuaSBUw6RnIHpydWdnIGJsaWNrYSB1bmQgw7xzIG5vIHVmIGRhIGxldHNjaCBUYWcgZnJldWEu",
-        )}
-      </p>
-      <p>
-        {decodeBase64(
-          "SSBnbsO8c3MgamVkYSBNb21lbnQgd29uaSBtaXQgZGVyIGTDtnJmIHZlcmJyaW5nYSwgZ2FkIHdlbm4gaSB3aSBldHogYSBrbGkgZ3N0cmVzc3QgYmkgdnVtIFNjaGFmZmEgdW5kIGVtIFN0dWRpdW0uIEkgZnJldSBtaSB1aCBmZXNjaHQgdWZkIEZlcmkgaW0gRmVicnVhciwgZGEgd8O2cmQgc2VjaGVyIHVoIHNjaMO8YS4=",
-        )}
-      </p>
-      <p>
-        {decodeBase64(
-          "TWl0IGRlbSBLYWxlbmRlciB3w7ZyIGkgZGVyIGdlcm4gZCBBZHZlbnRzeml0IGEga2xpIHZlcnPDvGVzc2EgdW5kIGhvZmYgZGFzIHNpIGthc2NoIGduw7xzc2EsIGJldm9yZCBkZW5uIGkgZGkgbGFuZ2EgV2llbmFjaHRzZmVyaSBrYXNjaCBnby4gSSBsaWViIGRpaSBtaW4gU2NoYXR6LCB1bmQgaG9mZiBkYXMgbWVyIG5vIGdhbnogdmllbCBXdWNoZWVuZCBkbyBpbSBTY2h3aXplcmhvZiBrb25uZCB2ZXJicmluZ2Eu",
-        )}
-      </p>
-      <p>{decodeBase64("RGluIE5pbHM=")}</p>
     </div>
   );
 }
